f3b913cd987b448d2f4df45efc9f4c9d9ae77e98
Rennan <rennanmore@gmail.com>
2018-02-28 12:10:17 -0300

    qqq

---
+++ exercicios/ex/counter.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { bindActionCreator } from 'redux'
+import { connect } from 'react-redux'
+import { inc, dec, stepChanger } from './counterActions'
+
+const Counter = props => (
+  <div>OK</div>
+)
+
+export default Counter
