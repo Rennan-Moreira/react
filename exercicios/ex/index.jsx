eaba2bf0df08345f3eeb696be01adbe3de3574d3
Rennan <rennanmore@gmail.com>
2018-02-28 09:24:53 -0300

    ok

--- exercicios/ex/index.jsx
+++ exercicios/ex/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ClassComponent from './classComponent'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import Field from './field'
+
+const reducers = combineReducers({
+  field: () => ({ value: 'Opa' })
+})
 
 ReactDOM.render(
-  <ClassComponent label='Contador' initialValue={10} />
-  ,document.getElementById('app'))
-
+  <Provider store={createStore(reducers)}>
+    <Field initialValue='Teste' />
+  </Provider>
+, document.getElementById('app'))
